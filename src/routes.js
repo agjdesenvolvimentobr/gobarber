20a8fb544ce4ddc2927037e0b8a7b0c7fd35c290
Amilson Gaspar <agjdesenvolvimento@gmail.com>
2019-10-20 00:52:18 -0300

    Criado primeira Migration e model.
    Criado a migration de criaçao da tabela de usuarios, alem de criar o
    model User e a configuraçao do loader.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Word!' }));
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello Word!' });
+});
+
+routes.get('/user', async (req, res) => {
+  const user = await User.create({
+    name: 'Amilson Gaspar',
+    email: 'agjdesenvilvimento@gmail.com',
+    password_hash: 'asdfasdf',
+  });
+  return res.json(user);
+});
 
 export default routes;

66e05302b1bee1698b2c9148c65ff5f07d332de7
Amilson Gaspar <agjdesenvolvimento@gmail.com>
2019-10-28 16:50:14 -0300

    Criado carregamento de arquivos e relacionamento com o usuario com o avatar.

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import authMiddleware from './app/middleware/auth';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,8 +20,6 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json(req.file);
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;

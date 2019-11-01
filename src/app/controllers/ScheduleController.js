3dbea5bbf02c975ed35c1bf448502e75fe2e6fc0
Amilson Gaspar <agjdesenvolvimento@gmail.com>
2019-11-01 18:49:13 -0300

    Criado listagem de agendamento para provider.

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,45 @@
+import { startOfDay, endOfDay, parseISO, isBefore } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointments from '../models/Appointment';
+import User from '../models/User';
+
+class ScheduleController {
+  async index(req, res) {
+    /**
+     * Check user is provider
+     */
+    const checkUserProvider = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true,
+      },
+    });
+    if (!checkUserProvider) {
+      res.status(401).json({ error: 'User is not provider!' });
+    }
+
+    const { date } = req.query;
+    const ISODate = parseISO(date);
+    const appointments = await Appointments.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(ISODate), endOfDay(ISODate)],
+        },
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name'],
+        },
+      ],
+    });
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();

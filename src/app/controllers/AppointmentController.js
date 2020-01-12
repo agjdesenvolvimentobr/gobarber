4dce08bd4ae5d66b0b71bf77b0f1b5eff5ce7089
Amilson Gaspar <agjdesenvolvimento@gmail.com>
2020-01-12 16:57:09 -0300

    Add horarios disponivies e tratamento de exceçoes.
    Criado rotas para listar os horarios disponiveis para atendimento de uma
    determinado provedor. Alem de criar atratamento de exceçoes de erros com
    sentry.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -19,7 +19,7 @@ class AppointmentControle {
       },
       limit: 20,
       offset: (page - 1) * 20,
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       order: ['date'],
       include: [
         {
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://05e8cba6c9cb43d098885ec30fe9a2e8@sentry.io/1878898',
+};
